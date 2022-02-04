c1f7a267e42e83a5b7cc6aaa416db4128d918fc8
Diego <drbv27@gmail.com>
2022-02-04 16:07:10 -0500

    next routing

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 import styled from "@emotion/styled";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Nosotros() {
   return (
     <div>
       <Layout>
-        <h1>Inicio</h1>
+        <h1>Nosotros</h1>
       </Layout>
     </div>
   );
