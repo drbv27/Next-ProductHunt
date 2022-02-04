cedf95ee0be7ee0252757e4e0f4a76047fca52e3
Diego <drbv27@gmail.com>
2022-02-04 15:49:20 -0500

    adding emotion styled

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
+import styled from "@emotion/styled";
 import styles from "../styles/Home.module.css";
+
+const Heading = styled.h1`
+  color: green;
+`;
 
 export default function Home() {
   return (
     <div>
-      <h1>Inicio</h1>
-      <style jsx>
-        {`
-          h1 {
-            color: red;
-          }
-        `}
-      </style>
+      <Heading>Inicio</Heading>
     </div>
   );
 }
