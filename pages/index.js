fc845701e6ce9621986b37cdceae428d94c35ac2
Diego <drbv27@gmail.com>
2022-02-04 15:15:52 -0500

    next css syntax

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,13 @@ export default function Home() {
   return (
     <div>
       <h1>Inicio</h1>
+      <style jsx>
+        {`
+          h1 {
+            color: red;
+          }
+        `}
+      </style>
     </div>
   );
 }
