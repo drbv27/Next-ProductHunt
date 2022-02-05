1c9040bca4897505d954b0d878e4a0d1fb8c1c5a
Diego <drbv27@gmail.com>
2022-02-04 19:42:05 -0500

    adding google fonts, and more

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import { jsx, css, Global, ClassNames } from "@emotion/react";
+import Head from "next/head";
 
 const Layout = (props) => {
   return (
@@ -24,12 +25,21 @@ const Layout = (props) => {
           body {
             font-size: 1.6rem;
             line-height: 1.5;
+            font-family: "PT Sans", sans-serif;
           }
           h1,
           h2,
           h3 {
             margin: 0 0 2rem 0;
             line-height: 1.5;
+          }
+          h1,
+          h2 {
+            font-family: "Roboto Slab", serif;
+            font-weight: 700;
+          }
+          h3 {
+            font-family: "PT Sans", sans-serif;
           }
           ul {
             list-style: none;
@@ -41,6 +51,24 @@ const Layout = (props) => {
           }
         `}
       />
+      <Head>
+        <html lang="es" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+        <title>Product Hunt Firebase y Next</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link href="/static/css/app.css" rel="stylesheet" />
+      </Head>
       <Header />
       <main>{props.children}</main>
     </>
