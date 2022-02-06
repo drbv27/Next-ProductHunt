b6c9ef9d91f3b69413d4dc3e33571219547ecaa3
Diego <drbv27@gmail.com>
2022-02-06 08:04:29 -0500

    adding more to custom hook

--- hooks/useValidacion.js
+++ hooks/useValidacion.js
@@ -1,23 +1,43 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
-const useValidacion = (stateInicial,validar,fn) => {
+const useValidacion = (stateInicial, validar, fn) => {
+  const [valores, guardarValores] = useState(stateInicial);
+  const [errores, guardarErrores] = useState({});
+  const [submitForm, guardarSubmitForm] = useState(false);
 
-  const [valores,guardarValores] = useState(stateInicial);
-  const [errores,guardarErrores] = useState({});
-  const [submitForm,guardarSubmitForm] = useState(false);
-
-  useEffect(() =>{
-    if(submitForm){
+  useEffect(() => {
+    if (submitForm) {
       const noErrores = Object.keys(errores).length === 0;
-      if(noErrores){
-        fn();//fn es la funcion que se ejecuta en el componente
+      if (noErrores) {
+        fn(); //fn es la funcion que se ejecuta en el componente
       }
-      guardarSubmitForm(false)
+      guardarSubmitForm(false);
     }
-  },[])
+  }, []);
 
+  //funcion que se ejecuta conforme el usuario escribe algo
+  const handleChange = (e) => {
+    guardarValores({
+      ...valores,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  return (  );
-}
- 
+  //funcion que se ejecuta cuando el usuario hace submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const erroresValidacion = validar(valores);
+    guardarErrores(erroresValidacion);
+    guardarSubmitForm(true);
+  };
+
+  return {
+    valores,
+    errores,
+    submitForm,
+    handleSubmit,
+    handleChange,
+  };
+};
+
 export default useValidacion;
