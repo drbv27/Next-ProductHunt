07dc7ad0042bb55604292c6e84485f3d1b3c3206
Diego <drbv27@gmail.com>
2022-02-06 07:22:36 -0500

    adding crear cuenta form

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Layout from "../components/layout/Layout";
 import Image from "next/image";
 import styled from "@emotion/styled";
@@ -7,7 +8,39 @@ export default function CrearCuenta() {
   return (
     <div>
       <Layout>
-        <h1>Crear Cuenta</h1>
+        <>
+          <h1>Crear Cuenta</h1>
+          <form>
+            <div>
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                id="nombre"
+                placeholder="Tu nombre"
+                name="nombre"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                placeholder="Tu email"
+                name="email"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Tu password"
+                name="password"
+              />
+            </div>
+            <input type="submit" value="Crear Cuenta" />
+          </form>
+        </>
       </Layout>
     </div>
   );
