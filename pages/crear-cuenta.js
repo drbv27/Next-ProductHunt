86a05de43a2452e369e572e7d4d0529892e8a4c7
Diego <drbv27@gmail.com>
2022-02-06 07:43:39 -0500

    adding css to crear cuenta form

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -2,16 +2,24 @@ import React from "react";
 import Layout from "../components/layout/Layout";
 import Image from "next/image";
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 import styles from "../styles/Home.module.css";
+import { Formulario, Campo, InputSubmit } from "../components/ui/Formulario";
 
 export default function CrearCuenta() {
   return (
     <div>
       <Layout>
         <>
-          <h1>Crear Cuenta</h1>
-          <form>
-            <div>
+          <h1
+            css={css`
+              text-align: center;
+            `}
+          >
+            Crear Cuenta
+          </h1>
+          <Formulario>
+            <Campo>
               <label htmlFor="nombre">Nombre</label>
               <input
                 type="text"
@@ -19,8 +27,8 @@ export default function CrearCuenta() {
                 placeholder="Tu nombre"
                 name="nombre"
               />
-            </div>
-            <div>
+            </Campo>
+            <Campo>
               <label htmlFor="email">Email</label>
               <input
                 type="text"
@@ -28,8 +36,8 @@ export default function CrearCuenta() {
                 placeholder="Tu email"
                 name="email"
               />
-            </div>
-            <div>
+            </Campo>
+            <Campo>
               <label htmlFor="password">Password</label>
               <input
                 type="password"
@@ -37,9 +45,9 @@ export default function CrearCuenta() {
                 placeholder="Tu password"
                 name="password"
               />
-            </div>
-            <input type="submit" value="Crear Cuenta" />
-          </form>
+            </Campo>
+            <InputSubmit type="submit" value="Crear Cuenta" />
+          </Formulario>
         </>
       </Layout>
     </div>
