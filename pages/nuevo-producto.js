0f61b8e4958dfa97492e74b314e10b5cd490f6ec
Diego <drbv27@gmail.com>
2022-02-08 07:23:16 -0500

    creating the new product form

--- pages/nuevo-producto.js
+++ pages/nuevo-producto.js
@@ -1,13 +1,135 @@
+import React, { useState } from "react";
 import Layout from "../components/layout/Layout";
 import Image from "next/image";
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
+import {
+  Formulario,
+  Campo,
+  InputSubmit,
+  Error,
+} from "../components/ui/Formulario";
+
+import firebase from "../firebase/firebase";
+
+//validaciones
+import useValidacion from "../hooks/useValidacion";
+import validarCrearCuenta from "../validacion/validarCrearCuenta";
+
+const STATE_INICIAL = {
+  nombre: "",
+  empresa: "",
+  imagen: "",
+  url: "",
+  descripcion: "",
+};
 
 export default function NuevoProducto() {
+  const router = useRouter();
+  const [error, guardarError] = useState(false);
+
+  const { valores, errores, handleSubmit, handleChange, handleBlur } =
+    useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
+
+  const { nombre, empresa, imagen, url, descripcion } = valores;
+
+  async function crearCuenta() {}
+
   return (
     <div>
       <Layout>
-        <h1>Nuevo Producto</h1>
+        <>
+          <h1
+            css={css`
+              text-align: center;
+            `}
+          >
+            Nuevo Producto
+          </h1>
+          <Formulario onSubmit={handleSubmit} noValidate>
+            <fieldset>
+              <legend>Información General</legend>
+              <Campo>
+                <label htmlFor="nombre">Nombre</label>
+                <input
+                  type="text"
+                  id="nombre"
+                  placeholder="Tu nombre"
+                  name="nombre"
+                  value={nombre}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+
+              {errores.nombre && <Error>{errores.nombre}</Error>}
+
+              <Campo>
+                <label htmlFor="empresa">Empresa</label>
+                <input
+                  type="text"
+                  id="empresa"
+                  placeholder="Nombre empresa"
+                  name="empresa"
+                  value={empresa}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+
+              {errores.empresa && <Error>{errores.empresa}</Error>}
+
+              <Campo>
+                <label htmlFor="imagen">Imagen</label>
+                <input
+                  type="file"
+                  id="imagen"
+                  name="imagen"
+                  value={imagen}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+
+              {errores.imagen && <Error>{errores.imagen}</Error>}
+
+              <Campo>
+                <label htmlFor="url">URL</label>
+                <input
+                  type="url"
+                  id="url"
+                  name="url"
+                  value={url}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+
+              {errores.url && <Error>{errores.url}</Error>}
+            </fieldset>
+            <fieldset>
+              <legend>Sobre tu producto</legend>
+              <Campo>
+                <label htmlFor="descripcion">Descripción</label>
+                <textarea
+                  id="descripcion"
+                  name="descripcion"
+                  value={descripcion}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </Campo>
+
+              {errores.descripcion && <Error>{errores.descripcion}</Error>}
+            </fieldset>
+
+            {error && <Error>{error}</Error>}
+
+            <InputSubmit type="submit" value="Crear Producto" />
+          </Formulario>
+        </>
       </Layout>
     </div>
   );
